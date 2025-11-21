51988857ebd3a74324315b367b4f1208c499b137
Caio Labella <caiolabella2016@gmail.com>
2025-11-21 15:59:50 -0300

    feat: implement initial web application structure, SEO assets, and sidebar component.

--- web/src/components/sidebar.tsx
+++ web/src/components/sidebar.tsx
@@ -7,10 +7,10 @@ export function Sidebar() {
     <div className="flex h-screen flex-col">
       <div className="flex items-center justify-between border-b border-zinc-700 px-4 py-5">
         <div className="flex items-baseline">
-          <span className="text-2xl font-semibold text-zinc-100">
+          <span className="text-xl font-semibold text-zinc-100">
             {'<'} Hook
           </span>
-          <span className="text-2xl font-normal text-zinc-400">In {'/>'}</span>
+          <span className="text-xl font-normal text-zinc-400">In {'/>'}</span>
         </div>
         <CreateEndpointButton />
       </div>
